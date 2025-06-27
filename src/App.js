814f3e6a4f11f9a95192bbef43535b5568004832
shota-yano <44024424+shota-yano@users.noreply.github.com>
2025-06-27 15:31:04 +0900

    Add menu list display from local JSON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch('/menu.json')
+      .then((res) => res.json())
+      .then((data) => setMenu(data))
+      .catch((err) => console.error('Failed to load menu:', err));
+  }, []);
+
   return (
     <div className="App">
       <h1>Welcome to Bento Order</h1>
+      <h2>Menu</h2>
+      <ul>
+        {menu.map((item, index) => (
+          <li key={index}>
+            {item.name} - {item.price}円
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
